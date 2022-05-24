25f82739f9a99b4ce00802094008f7f27902c093
Amir Zolfaghari <cvemys@gmail.com>
2022-05-24 23:08:31 +0430

    removing extra fields from constructors

--- contracts/tokenA.sol
+++ contracts/tokenA.sol
@@ -5,10 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract tokenA is ERC20 {
 
-    constructor(address a1, address a2) ERC20("tokenA","AAA"){
-        _mint(msg.sender,1000);
-        _mint(a1,100);
-        _mint(a2,100);
+    constructor() ERC20("tokenA","AAA"){
+        _mint(msg.sender,1000000);
     }
 
 }
